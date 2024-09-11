a72124e89ba1cb74b1ea8441f62f093c309f71b9
Praveenakalai <rpraveenaece@gmail.com>
2024-09-11 12:38:16 +0530

    pravee code

--- src/test/java/org/test/SampleTest.java
+++ src/test/java/org/test/SampleTest.java
@@ -3,7 +3,10 @@ package org.test;
 public class SampleTest {
 	public static void main(String[] args) {
 		System.out.println("Test");
-		//here start to write implementations
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+		
 	}
 
 }
